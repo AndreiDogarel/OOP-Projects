4c7ea0bc086c59672ae35782b449ba73ae4b8245
Andrei Dogarel <75136445+AndreiKatsuki@users.noreply.github.com>
2023-04-16 18:57:04 +0300

    update

--- Project_2_DatingApp/132_Dogare_Andrei_Proiect_2.cpp
+++ Project_2_DatingApp/132_Dogare_Andrei_Proiect_2.cpp
@@ -2,82 +2,167 @@
 
 using namespace std;
 
-class IOSystem{
-    virtual void citire(istream& in) = 0;
-    virtual void afisare(ostream& out) const = 0;
+class IOInterface {
+public:
+    virtual istream& citire(istream&) = 0;
+    virtual ostream& afisare(ostream&) const = 0;
 };
 
-class User : public IOSystem{
-    protected:
-        string numeUtilizator;
-        string nume;
-        string prenume;
-        string email;
-        string parola;
-        int varsta;
-        int range;      /// raza de cautare pentru match-uri
-        bool restrict;
-    
-    public:
-        virtual void autentificare(istream& in){
-            cout << "Nume: ";
-            in >> this->nume;
+class Job : public IOInterface{
+protected:
+    string numeJob;
+    int salariuDeBaza;
+    struct locatie {
+        string numeTara;
+        string numeOras;
+        string numeStrada;
+        int nrStrada;
+    } locatieJob;
+    int nrAniExperientaMin;
+    vector<string> limbajeCerute;
+    bool easyApply;
+public:
+    //------- CONSTRUCTORI -------//
+    Job();
+    Job(string numeJob, int salariuDeBaza, locatie locatieJob, int nrAniExperientaMin, const vector<string>& limbajeCerute, bool easyApply);
+    Job(const Job& obj);
 
-            cout << "Prenume: ";
-            in >> this->prenume;
+    //------- OPERATORI -------//
+    Job& operator = (const Job& obj);
+    istream& citire(istream& in);
+    ostream& afisare(ostream& out) const;
+    friend istream& operator >> (istream &in, Job &obj)
+    {
+        return obj.citire(in);
+    }
+    friend ostream& operator << (ostream& out, const Job& obj){
+        return obj.afisare(out);
+    }
 
-            cout << "Varsta: ";
-            in >> this->varsta;
-
-            cout << "E-Mail: ";
-            in >> this->email;
-
-            cout << "Parola: ";
-            in >> this->parola;
-
-            cout << "Afiseaza persoane la o distanta maxima de: ";
-            in >> this->range;
-
-            cout << "Doriti ca afisarea sa se realizeze doar in acesta raza?\n 1 -> Da\n 2 -> Nu\n";
-            int response;
-            cout << "Raspuns: ";
-            in >> response;
-            if(response == 1 || response == 2){
-                if(response == 1){
-                    this->restrict = true;
-                }
-                else{
-                    this->restrict = false;
-                }
-            }
-            else{
-                while(true){
-                    cout << "Raspuns invalid, incercati inca o data: \n";
-                    cout << "Raspuns: ";
-                    in >> response;
-                    if(response == 1 || response == 2){
-                        if(response == 1){
-                            this->restrict = true;
-                        }
-                        else{
-                            this->restrict = false;
-                        }
-                        break;
-                    }
-                }
-            }
-
-            cout << "Sub ce nume doresti sa apari celorlalti utilizatori?\n";
-            in >> this->numeUtilizator;
-        }
-
-        virtual void afisareProfil(ostream& out) const {
-            cout << this->numeUtilizator << "\n";
-            cout << "Varsta: " << this->varsta << "\n";
-            
-        }
+    //------- DESTRUCTOR -------//
+    virtual ~Job();
 };
+
+Job::Job(){
+    this->numeJob = "";
+    this->salariuDeBaza = 0;
+    this->locatieJob.nrStrada = 0;
+    this->locatieJob.numeOras = "";
+    this->locatieJob.numeStrada = "";
+    this->locatieJob.numeTara = "";
+    this->nrAniExperientaMin = 0;
+    this->limbajeCerute = {};
+    this->easyApply = false;
+}
+
+Job::Job(string numeJob, int salariuDeBaza, locatie locatieJob, int nrAniExperientaMin, const vector<string>& limbajeCerute, bool easyApply){
+    this->numeJob = numeJob;
+    this->salariuDeBaza = salariuDeBaza;
+    this->locatieJob = locatieJob;
+    this->nrAniExperientaMin = nrAniExperientaMin;
+    this->limbajeCerute = limbajeCerute;
+    this->easyApply = easyApply;
+}
+
+Job::Job(const Job& obj){
+    this->numeJob = obj.numeJob;
+    this->salariuDeBaza = obj.salariuDeBaza;
+    this->locatieJob = obj.locatieJob;
+    this->nrAniExperientaMin = obj.nrAniExperientaMin;
+    this->limbajeCerute = obj.limbajeCerute;
+    this->easyApply = obj.easyApply;
+}
+
+Job& Job::operator = (const Job& obj){
+    if(this != &obj){
+        this->numeJob = obj.numeJob;
+        this->salariuDeBaza = obj.salariuDeBaza;
+        this->locatieJob = obj.locatieJob;
+        this->nrAniExperientaMin = obj.nrAniExperientaMin;
+        this->limbajeCerute = obj.limbajeCerute;
+        this->easyApply = obj.easyApply;
+    }
+    return *this;
+}
+
+istream& Job::citire(istream& in){
+    cout << "Numele Job-ului: ";
+    in >> this->numeJob;
+
+    cout << "Salariu: ";
+    in >> this->salariuDeBaza;
+
+    cout << "Locatia Job-ului:\n";
+    cout << " Tara: ";
+    in >> this->locatieJob.numeTara;
+    cout << " Oras: ";
+    in >> this->locatieJob.numeOras;
+    cout << " Strada: ";
+    in >> this->locatieJob.numeStrada;
+    cout << " Numarul Strazii: ";
+    in >> this->locatieJob.nrStrada;
+
+    cout << "Numarul minim de ani de experienta necesari: ";
+    in >> this->nrAniExperientaMin;
+
+    cout << "Limbajele de programare necesare job-ului: ";
+    string limbaje;
+    in.get();
+    getline(in, limbaje);
+    stringstream s(limbaje);
+    while(getline(s, limbaje, ' ')){
+        this->limbajeCerute.push_back(limbaje);
+    }
+
+    cout << "Candidatura simpla?\n 1 -> Da\n 2 -> Nu\n";
+    int response;
+    cout << "Raspuns: ";
+    in >> response;
+    if(response == 1){
+        this->easyApply = true;
+    }
+    else{
+        this->easyApply = false;
+    }
+
+    return in;
+}
+
+ostream& Job::afisare(ostream& out) const {
+    out << "Nume Job: " << this->numeJob << "\n";
+    out << "Salariu de baza: " << this->salariuDeBaza << "\n";
+    out << "Locatie Job: " << this->locatieJob.numeTara << ", " << this->locatieJob.numeOras << ", Strada " << this->locatieJob.numeStrada << ", Numarul " << this->locatieJob.nrStrada << "\n";
+    out << "Numar minim de ani de experienta: " << this->nrAniExperientaMin << "\n";
+    out << "Limbaje de programare necesare: ";
+    for(auto it : this->limbajeCerute){
+        out << it << " ";
+    }
+    out << "\n";
+    out << "Candidatura simpla: ";
+    if(this->easyApply){
+        out << "Da\n";
+    }
+    else{
+        out << "Nu\n";
+    }
+
+    return out;
+}
+
+Job::~Job(){
+    this->numeJob = "";
+    this->salariuDeBaza = 0;
+    this->locatieJob.nrStrada = 0;
+    this->locatieJob.numeOras = "";
+    this->locatieJob.numeStrada = "";
+    this->locatieJob.numeTara = "";
+    this->nrAniExperientaMin = 0;
+    this->limbajeCerute.clear();
+    this->easyApply = false;
+}
 
 int main(){
-
+    Job j;
+    cin >> j;
+    cout << j;
 }
